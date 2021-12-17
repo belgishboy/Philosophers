7ed847ca82012c2d8163b165aff61f9c64f8e8e6
Vincent Heymans <vheymans@c2s17d101.42wolfsburg.de>
2021-12-17 19:42:17 +0100

    is this going to commit everything?

---
+++ philo/src/print.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vheymans <vheymans@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/17 16:33:32 by vheymans          #+#    #+#             */
+/*   Updated: 2021/12/17 17:07:39 by vheymans         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../philo.h"
+
+/*
+**
+*/
+void	prt_status(t_philo *phil, t_table *t, int status)
+{
+	pthread_mutex_lock(&t->prt);
+	printf("%lld %d ", t->ct - t->st, phil->p_n + 1);
+	if (status == 1)
+		printf("has taken a fork\n");
+	else if (status == 2)
+		printf("is eating\n");
+	else if (status == 3)
+		printf("is sleeping\n");
+	else if (status == 4)
+		printf("is thinking\n");
+	else if (status == 5)
+		printf("died\n");
+	pthread_mutex_unlock(&t->prt);
+}
